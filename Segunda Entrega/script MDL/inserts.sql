a087da1d5f30ded26761c987d4155e05fd8e3a2d
Oseas1995 <oseasmejia95@yahoo.es>
2019-04-05 21:38:29 -0600

    script DML

---
+++ Segunda Entrega/script MDL/inserts.sql
@@ -0,0 +1,215 @@
+
+------------------ INSERT TELEFONO ------------------
+
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (1,'2090-7146',1);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (2,'3985-5240',2);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (3,'2128-3334',3);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (4,'5873-4378',4);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (5,'2999-5807',5);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (6,'1927-9969',6);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (7,'2743-8604',7);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (8,'4741-9356',8);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (9,'9373-0521',9);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (10,'6061-5791',10);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (11,'5945-1617',11);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (12,'9069-9902',12);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (13,'8284-3627',13);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (14,'6089-9021',14);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (15,'4137-8081',15);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (16,'1989-7983',16);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (17,'5898-0481',17);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (18,'1254-0038',18);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (19,'4512-7683',19);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (20,'3642-1573',20);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (21,'1075-6617',21);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (22,'4940-7021',22);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (23,'3486-2747',23);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (24,'1914-9662',24);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (25,'2756-6682',25);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (26,'6428-4005',26);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (27,'7932-8061',27);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (28,'1197-5857',28);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (29,'2597-5193',29);
+INSERT INTO TELEFONO (idTelefono,numero,Persona_idPersona) VALUES (30,'1033-6228',30);
+
+
+------------------ INSERT PERSONA ------------------
+
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (1,'Lionel','Ruby','Mechelle','Mannix','Apdo.:419-8537 Proin Ctra.','Lionel.Mechelle@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (2,'Barrett','Katelyn','Kameko','Oscar','Apdo.:165-4262 Non C.','Barrett.Kameko@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (3,'Wade','Ivana','Melissa','Ferdinand','Apartado núm.: 905, 1658 Phasellus C.','Wade.Melissa@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (4,'Malcolm','Angelica','Uriel','Mannix','Apdo.:984-4984 Diam C.','Malcolm.Uriel@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (5,'Sawyer','Amelia','Benjamin','Fuller','Apdo.:602-6808 Sed Calle','Sawyer.Benjamin@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (6,'Austin','Karly','Aladdin','Aristotle','Apartado núm.: 247, 1371 Curabitur Carretera','Austin.Aladdin@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (7,'Silas','Vivian','Hunter','Jermaine','435-7991 Commodo Avenida','Silas.Hunter@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (8,'Elvis','Kylie','Gage','Abbot','265-7874 Felis. Ctra.','Elvis.Gage@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (9,'Dolan','Willa','Scarlet','Simon','Apartado núm.: 465, 4861 Blandit. Avenida','Dolan.Scarlet@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (10,'Kasimir','Jacqueline','Rajah','Barrett','Apdo.:555-5138 Ante C/','Kasimir.Rajah@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (11,'Mannix','Mara','Ashton','Acton','Apartado núm.: 955, 5906 Erat. Calle','Mannix.Ashton@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (12,'Rajah','Vivian','Damian','Damian','Apdo.:199-8231 Enim Avenida','Rajah.Damian@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (13,'Lyle','Aspen','Zenaida','Elliott','Apdo.:851-1894 Interdum Calle','Lyle.Zenaida@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (14,'Josiah','Francesca','Tara','Logan','Apartado núm.: 625, 7156 Cursus Av.','Josiah.Tara@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (15,'Caldwell','Chanda','Colette','William','Apartado núm.: 447, 6839 Dolor. ','Caldwell.Colette@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (16,'Carlos','Lacy','Catherine','Kenneth','275-594 Proin Avenida','Carlos.Catherine@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (17,'Ali','Nerea','Hop','Lamar','Apdo.:279-8210 Conubia Carretera','Ali.Hop@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (18,'Burton','Xena','Wing','Cade','Apartado núm.: 405, 6350 Semper Ctra.','Burton.Wing@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (19,'Kane','Mikayla','Barrett','Donovan','Apartado núm.: 739, 2415 Egestas C.','Kane.Barrett@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (20,'Nissim','Lenore','Griffin','Quamar','Apdo.:533-8845 Eleifend, Ctra.','Nissim.Griffin@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (21,'Porter','Kerry','Hedley','Jarrod','2752 Tellus Calle','Porter.Hedley@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (22,'Paki','Sade','Cheryl','Kieran','266-3845 Bibendum Ctra.','Paki.Cheryl@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (23,'Wayne','Sydnee','Gray','Arthur','529-2933 Convallis Avda.','Wayne.Gray@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (24,'Tyler','Priscilla','Carolyn','Brian','931-1471 Id, ','Tyler.Carolyn@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (25,'Marshall','Jasmine','Jael','Bradley','483-5240 Mauris, C/','Marshall.Jael@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (26,'Wyatt','Hanna','Olivia','Stuart','Apdo.:976-9925 Proin Av.','Wyatt.Olivia@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (27,'Cody','Maggie','Lilah','Gage','Apdo.:696-4729 Id Calle','Cody.Lilah@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (28,'Gage','Briar','Dylan','Chester','Apartado núm.: 475, 9258 Donec Av.','Gage.Dylan@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (29,'Aristotle','Mary','Erich','Kirk','Apartado núm.: 308, 1177 Tristique Calle','Aristotle.Erich@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (30,'Louis','Lucy','Dexter','Josiah','376-9729 Sed Avda.','Louis.Dexter@gmail.com');
+INSERT INTO PERSONA (idPersona,pnombre,snombre,papellido,sapellido,direccion,correo) VALUES (31,'Gage','Lee','Lucian','Honorato','Apdo.:658-265 Lacinia C/','Gage.Lucian@gmail.com');
+
+
+------------------ INSERT CARGO ------------------
+
+INSERT INTO CARGO (idCargo,descripcion) VALUES (1,'Director');
+INSERT INTO CARGO (idCargo,descripcion) VALUES (2,'Subdirector');
+INSERT INTO CARGO (idCargo,descripcion) VALUES (3,'Administrador');
+INSERT INTO CARGO (idCargo,descripcion) VALUES (4,'Jefe cajero');
+INSERT INTO CARGO (idCargo,descripcion) VALUES (5,'Cajero');
+INSERT INTO CARGO (idCargo,descripcion) VALUES (6,'Personal de aseo');
+INSERT INTO CARGO (idCargo,descripcion) VALUES (7,'Atencion al cliente');
+INSERT INTO CARGO (idCargo,descripcion) VALUES (8,'Guardia de seguridad');
+
+------------------ INSERT IDIOMA ------------------
+
+INSERT INTO IDIOMA (idIdioma,descripcion) VALUES (1,'Ingles');
+INSERT INTO IDIOMA (idIdioma,descripcion) VALUES (2,'Español');
+
+------------------ INSERT AUTOR ------------------
+
+
+
+
+------------------ INSERT Editorial ------------------
+
+INSERT INTO EDITORIAL (idEditorial,nombre,correo,direccion) VALUES (1,'Larrosa Mas, S.L.','larrosa.@gmail.com','León XIII, 12');
+INSERT INTO EDITORIAL (idEditorial,nombre,correo,direccion) VALUES (2,'Deloria Editores','deloriaeditores@gmail.com','Gran Vía, 77');
+INSERT INTO EDITORIAL (idEditorial,nombre,correo,direccion) VALUES (3,'TechniBooks','technibooks@gmail.com','Espronceda, 11');
+INSERT INTO EDITORIAL (idEditorial,nombre,correo,direccion) VALUES (4,'McCoy Hill','mccoyhill@gmail.com','Sants Metges, 2');
+INSERT INTO EDITORIAL (idEditorial,nombre,correo,direccion) VALUES (5,'Grisham Publishing','grishampublishing@gmail.com','Alfonso Paso, 3');
+INSERT INTO EDITORIAL (idEditorial,nombre,correo,direccion) VALUES (6,'McGraw-Hill','mcgrawhill@gmail.com','San Andrés, 21');
+INSERT INTO EDITORIAL (idEditorial,nombre,correo,direccion) VALUES (7,'Springer','springer@gmail.com','Carlos III,43');
+INSERT INTO EDITORIAL (idEditorial,nombre,correo,direccion) VALUES (8,'Prentice Hall','prenticehall@gmail.com','Plaza Luceros, 4');
+INSERT INTO EDITORIAL (idEditorial,nombre,correo,direccion) VALUES (9,'Universidad de Granada','universidadgranada@gmail.com','Mairena del Aljarafe (Sevilla)');
+INSERT INTO EDITORIAL (idEditorial,nombre,correo,direccion) VALUES (10,'Addison Wesley','addisonwesley@gmail.com','Jerez de la Frontera (Cádiz).');
+
+
+------------------ INSERT CATEGORIA ------------------
+
+INSERT INTO CATEGORIA (idCategoria,descripcion) VALUES (1,'Psicología');
+INSERT INTO CATEGORIA (idCategoria,descripcion) VALUES (2,'Ciencia');
+INSERT INTO CATEGORIA (idCategoria,descripcion) VALUES (3,'Salud');
+INSERT INTO CATEGORIA (idCategoria,descripcion) VALUES (4,'Ficción');
+INSERT INTO CATEGORIA (idCategoria,descripcion) VALUES (5,'Novela');
+INSERT INTO CATEGORIA (idCategoria,descripcion) VALUES (6,'Poesía');
+INSERT INTO CATEGORIA (idCategoria,descripcion) VALUES (7,'Técnica');
+INSERT INTO CATEGORIA (idCategoria,descripcion) VALUES (8,'Historia');
+INSERT INTO CATEGORIA (idCategoria,descripcion) VALUES (9,'Filosofía');
+INSERT INTO CATEGORIA (idCategoria,descripcion) VALUES (10,'Arte');
+INSERT INTO CATEGORIA (idCategoria,descripcion) VALUES (11,'Matemática');
+
+
+------------------ INSERT GENERO ------------------
+
+INSERT INTO GENERO (idGenero,descripcion) VALUES (1,'Romántica');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (2,'Aventura');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (3,'Terror');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (4,'Ficcion / Realidad');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (5,'Ciencia Ficción');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (6,'Investigación');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (7,'Biográfica');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (8,'Infantil');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (9,'Autoayuda');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (10,'Erótica');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (11,'Hogar');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (12,'Enciclopedia / Manual');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (13,'Política');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (14,'Economía / Marketing');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (15,'Sociedad');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (16,'Deportes');
+INSERT INTO GENERO (idGenero,descripcion) VALUES (17,'Viajes / Cultura');
+
+
+------------------ INSERT TIPO PAGO ------------------
+
+INSERT INTO TIPOPAGO (idTipoPago,descripcion) VALUES (1,'Tarjetas de Credito');
+INSERT INTO TIPOPAGO (idTipoPago,descripcion) VALUES (2,'Tarjetas de Debito');
+INSERT INTO TIPOPAGO (idTipoPago,descripcion) VALUES (3,'Efectivo');
+
+
+------------------ INSERT LIBRO ------------------
+
+
+
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (1,'Don Quijote de La Mancha I',1995,4,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (2,'El tránsito terreno',2015,2,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (3,'Historias de Nueva',2000,8,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (4,'El principito',1999,4,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (5,'El príncipe',1995,4,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (6,'Diplomacia',1995,8,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (7,'Los Windsor Plaza & Janés',1995,5,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (8,'El Último Emperador',1996,5,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (9,'Fortunata y Jacinta',1996,6,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (10,'Sistemas operativos',1990,7,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (11,'Poemas intrínsecos',2000,6,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (12,'Avances en Arquitectura',1990,2,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (13,'Las balas del bien',2001,4,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (14,'La mente y el sentir',2005,6,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (15,'Ensayos póstumos',1990,6,1);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (16,'Arquitectura y arte',2003,10,1);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (17,'Historia de Occidente',2014,8,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (18,'Sentimiento popular',2017,6,1);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (19,'Amigos o enemigos',1997,4,1);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (20,'Procesadores cuánticos',1899,2,1);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (21,'Canto de esperanza',2013,5,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (22,'Algebra lineal',2004,11,1);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (23,'Calculo II',2016,11,2);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (24,'Ecuaciones Diferenciales',2013,11,1);
+INSERT INTO LIBRO (idlibro,nombre,anioPublicacion,Categoria_idCategoria,Idioma_idIdioma) VALUES (25,'Estructuras de datos',2011,11,1);
+
+
+------------------ INSERT CLIENTE ------------------
+
+INSERT INTO CLIENTE (idCliente,fechaIngreso,Persona_idPersona) VALUES (1,'10/14/08',20);
+INSERT INTO CLIENTE (idCliente,fechaIngreso,Persona_idPersona) VALUES (2,'01/19/11',21);
+INSERT INTO CLIENTE (idCliente,fechaIngreso,Persona_idPersona) VALUES (3,'01/19/08',22);
+INSERT INTO CLIENTE (idCliente,fechaIngreso,Persona_idPersona) VALUES (4,'05/01/17',23);
+INSERT INTO CLIENTE (idCliente,fechaIngreso,Persona_idPersona) VALUES (5,'04/09/15',24);
+INSERT INTO CLIENTE (idCliente,fechaIngreso,Persona_idPersona) VALUES (6,'10/21/09',25);
+INSERT INTO CLIENTE (idCliente,fechaIngreso,Persona_idPersona) VALUES (7,'06/06/04',26);
+INSERT INTO CLIENTE (idCliente,fechaIngreso,Persona_idPersona) VALUES (8,'09/16/15',27);
+INSERT INTO CLIENTE (idCliente,fechaIngreso,Persona_idPersona) VALUES (9,'11/19/17',28);
+INSERT INTO CLIENTE (idCliente,fechaIngreso,Persona_idPersona) VALUES (10,'10/15/03',29);
+INSERT INTO CLIENTE (idCliente,fechaIngreso,Persona_idPersona) VALUES (11,'02/28/03',30);
+
+
+------------------ INSERT  ------------------
+
+
+
+
+------------------ INSERT  ------------------
+
+
+
+
+------------------ INSERT  ------------------
+
+
+
+
+------------------ INSERT  ------------------
+
+
+
+
+------------------ INSERT  ------------------
